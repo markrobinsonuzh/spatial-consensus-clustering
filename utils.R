f3eaecaded3574442cf1cb74feb2b48a476182b5
Mark Robinson <mark@MLS-M-MAROBI.local>
2024-10-02 09:35:18 +0200

    add utils.R

---
+++ utils.R
@@ -0,0 +1,144 @@
+calc_entropy <- function(u) {
+  p <- u[u>0]
+  p <- p/sum(p)
+  -sum(p*log(p))
+}
+
+
+# given a matrix of labels, calculate all pairwise ARIs
+calc_aris <- function(m) {
+  require(mclust)
+  a <- diag(ncol(m))
+  for(i in 1:(ncol(m)-1))
+    for(j in 2:ncol(m))
+      a[i,j] <- a[j,i] <- mclust::adjustedRandIndex(m[,i], m[,j])
+  rownames(a) <- colnames(a) <- colnames(m)
+  a
+}
+
+num_methods <- function(u) {
+  s <- sapply(strsplit(u, "[_\\.]"), .subset, 1)
+  g <- grep("label",s)
+  if(length(g)) 
+    s <- s[-g]
+  s %>% table %>% length
+}
+
+
+calc_summary <- function(tree, arism, k = 3, gtcol = "label") {
+  ct <- cutree(tree, k=k)
+  tct <- table(ct)
+  data.frame(group = names(tct), 
+             number = as.integer(tct),
+             ari_to_truth = sapply(names(tct),
+                                   function(u) median(arism[ct==u & names(ct)!=gtcol,gtcol])),
+             ari_block = sapply(names(tct),
+                                function(u) {z <- arism[ct==u,ct==u]; median(z[upper.tri(z)]) }),
+             k = k,
+             gt_in_group = (names(tct) == ct[gtcol]),
+             number_distinct_methods = sapply(names(tct),
+                                              function(u) num_methods(names(ct)[ct==u])))
+}
+
+
+align_classes <- function(d, ref) {
+  
+  bcs <- d %>% as.data.frame
+  for(j in 1:ncol(bcs))
+    bcs[,j] <- as.factor(bcs[,j])
+  levs <- lapply(bcs, levels)
+  n_levs <- sapply(levs, length)
+  
+  cols_to_change <- setdiff(colnames(bcs), ref)
+  
+  for(i in cols_to_change) {
+    if(n_levs[i] != n_levs[ref]) {
+      message(paste0(i," has ",n_levs[i],
+                     " levels; reference (", ref, 
+                     ") has ",n_levs[ref], " (not modifying)"))
+      next
+    }
+    hung <- clue::solve_LSAP(table(bcs[,ref], bcs[,i]), 
+                             maximum = TRUE)
+    lookup <- cbind(seq_along(hung), levs[[i]][hung])
+    levels(bcs[,i]) <- lookup[order(as.integer(lookup[,2])),1]
+    bcs[,i] <- as.factor(as.character(bcs[,i]))
+  }
+  
+  bcs
+  
+}
+
+spot_entropy <- function(spatial_coords, label, k=6) {
+  require(dbscan)
+  knns <- kNN(spatial_coords, k=k)
+  label <- as.factor(label)
+  neighb_labels <- apply(knns$id, 2, function(u) label[u])
+  apply(neighb_labels, 
+        1, function(u) calc_entropy(table(factor(u,
+                                                 levels=levels(label)))))
+}
+
+
+get_SpatialExperiment <- function(
+    feature_file,
+    observation_file,
+    coord_file,
+    matrix_file = NA,
+    reducedDim_file = NA,
+    assay_name = "counts",
+    reducedDim_name = "reducedDim") {
+  rowData <- read.delim(feature_file, stringsAsFactors = FALSE, row.names = 1)
+  colData <- read.delim(observation_file, stringsAsFactors = FALSE, row.names = 1)
+  
+  coordinates <- read.delim(coord_file, sep = "\t", row.names = 1)
+  coordinates <- as.matrix(coordinates[rownames(colData), ])
+  coordinates[,c(1:2)] <- as.numeric(coordinates[,c(1:2)])
+  
+  spe <- SpatialExperiment::SpatialExperiment(
+    rowData = rowData, colData = colData, spatialCoords = coordinates
+  )
+  
+  if (!is.na(matrix_file)) {
+    assay(spe, assay_name, withDimnames = FALSE) <- as(Matrix::t(Matrix::readMM(matrix_file)), "CsparseMatrix")
+    #assay(spe, "logcounts", withDimnames = FALSE) <- as(Matrix::t(Matrix::readMM(matrix_file)), "CsparseMatrix")
+  }
+  
+  # Filter features and samples
+  if ("selected" %in% colnames(rowData(spe))) {
+    spe <- spe[as.logical(rowData(spe)$selected), ]
+  }
+  if ("selected" %in% colnames(colData(spe))) {
+    spe <- spe[, as.logical(colData(spe)$selected)]
+  }
+  
+  if (!is.na(reducedDim_file)) {
+    dimRed <- read.delim(reducedDim_file, stringsAsFactors = FALSE, row.names = 1)
+    reducedDim(spe, reducedDim_name) <- as.matrix(dimRed[colnames(spe), ])
+  }
+  return(spe)
+}
+
+
+
+plotter_fun <- function(u,v,z, point_size = 3) {
+  
+  df <- data.frame(colData(spe)[,c("row","col")], 
+                   gene=logcounts(spe)[u,],
+                   ground_truth = gt)
+  df$highlight <- df$ground_truth %in% v
+  
+  
+  ggplot(df, aes(x=row, y=col, fill=gene, colour=highlight)) + 
+    geom_point(shape=21, size = point_size) +
+    theme_classic() +
+    # theme(legend.position = "none") +
+    xlab("") + ylab("") +
+    scale_colour_manual(values = gt_col) +
+    scale_fill_gradient(low="gray95", high="deeppink4") +
+    scale_x_reverse(expand = expansion(0,1)) +
+    scale_y_continuous(expand = expansion(0,1)) +
+    ggtitle(paste0(u,": ",z," (",paste0(v,collapse = ","),")"))
+}
+
+
